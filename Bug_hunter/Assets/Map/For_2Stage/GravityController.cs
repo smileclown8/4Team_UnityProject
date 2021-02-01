137204807bafbbac8e2b1f3700a688cb5e910069
smileclown8 <77978713+smileclown8@users.noreply.github.com>
2021-02-02 04:05:26 +0900

    2-2완료
    타일셋 및 중력장, 미끄러미 기믹 구현,
    백그라운드 배치 완료

---
+++ Bug_hunter/Assets/Map/For_2Stage/GravityController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    Rigidbody2D rb;
+
+
+
+    void Start()
+    {
+
+        player = GameObject.Find("player");
+        rb = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rb.gravityScale = -5;
+        }
+
+    }
+
+}
