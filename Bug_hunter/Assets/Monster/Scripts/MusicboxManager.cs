0e2fcf7787beb12c9ca35caa50458ff075dc33d2
writingbubo <writingbubo@gmail.com>
2021-02-01 18:10:52 +0900

    몬스터 작업 백업
    각각 애니메이션 콜라이더 수정됨. NPCmon 생성해서 작업중...
    몬스터 리소스 폴더에 추가됨

--- Bug_hunter/Assets/Monster/Scripts/MusicboxManager.cs
+++ Bug_hunter/Assets/Monster/Scripts/MusicboxManager.cs
@@ -14,22 +14,26 @@ public class MusicboxManager : MonoBehaviour
     GameObject player;                  // 그냥 플레이어
     GameObject target;                  // 끌어당길 플레이어
     Rigidbody2D targetrb;
-    float targetGravity;
     float targetGravity_original;       // 플레이어의 원래 중력값
     Transform targetTrans;              // 플레이어의 위치
     Transform myPos;                    // 오르골의 위치
     public float forceFactor;           // 당기는 힘값
 
-    float pulltime;
-    float cooltime = 1.5f;
+    [SerializeField] float pulltime;
+    [SerializeField] float cooltime = 1.5f;
+    float playerHP;
+    //float playerDef;
 
-    //Animator anim;
 
     void Start()
     {
         myPos = GetComponent<Transform>();
         player = GameObject.FindWithTag("Player");
         targetGravity_original = player.GetComponent<Rigidbody2D>().gravityScale;
+        playerHP = player.GetComponent<PlayerStatusManager>().player_HP;
+        // playerDef = player.GetComponent<PlayerStatusManager>().player_Def;  -------------- 추가할것
+
+        pulltime = 1.5f;
     }
 
     void Update()
@@ -43,24 +47,47 @@ public class MusicboxManager : MonoBehaviour
             targetTrans = target.GetComponent<Transform>();
             targetrb.gravityScale = 0.2f;
 
+
+
         }
         
         if (!isTracing)
         {
             player.GetComponent<Rigidbody2D>().gravityScale = targetGravity_original;
         }
-    }
 
-    void FixedUpdate()
-    {
         if (isTracing && target.gameObject.tag == "Player")
         {
-            if(pulltime >= cooltime)
+            if (pulltime >= cooltime)
             {
-                targetrb.AddForce((myPos.position - targetTrans.position) * forceFactor * Time.fixedDeltaTime);
+                targetrb.AddForce((myPos.position - targetTrans.position) * forceFactor * Time.deltaTime);
+                playerHP -= attack;
+                Debug.Log("빡! 플레이어 HP: " + playerHP + ", 데미지 " + attack);
+                pulltime = 0;
             }
             pulltime += Time.deltaTime;
         }
     }
 
+
+
+
+    SpriteRenderer spriteRenderer;
+    new CapsuleCollider2D collider;
+
+    // 체력 0 이하일 때 피격효과 내고 사라진다.
+    public void OnDamaged(int damage)
+    {
+        if (hp <= 0)
+        {
+            spriteRenderer.color = new Color(1, 1, 1, 0.3f);        // 맞으면 반투명해짐
+            collider.enabled = false;                               // 충돌 끄기
+            Invoke("Destroy", 1.5f);                                // 1.5초 뒤 사라짐
+        }
+    }
+
+    private void Destroy()
+    {
+        Destroy(gameObject);
+    }
 }
---
+++ Bug_hunter/Assets/Monster/Scripts/NPCmonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCmonManager : MonoBehaviour
+{
+    // 스탯
+    public int hp = 30;
+    public int attack = 4;
+    int buffRate = 30;
+
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField] float shoottime;       // 발사시간
+    [SerializeField] float nextshoot;       // 쿨타임
+
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        nextshoot = 2;
+    }
+
+    void Update()
+    {
+        
+    }
+}
