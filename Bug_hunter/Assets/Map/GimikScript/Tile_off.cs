89db425ec02e70dc940c34e6e55844c50a6e4170
smileclown8 <77978713+smileclown8@users.noreply.github.com>
2021-02-01 12:42:17 +0900

    2-0 완료
    타일 배치 및 기믹 배치 완료
    
    추가로 필요한 작업 : 기믹 데미지 및 아이템, 몬스터, 스토리 오브젝트 배치

---
+++ Bug_hunter/Assets/Map/GimikScript/Tile_off.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile_off : MonoBehaviour
+{
+    GameObject tilemap2_0_1;
+    Renderer tileColor2_0_1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap2_0_1 = GameObject.Find("Tilemap2_0_1");
+        if (tilemap2_0_1 != null)
+        {
+            tileColor2_0_1 = tilemap2_0_1.GetComponent<Renderer>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    // Trigger 타입의 콜라이더와 충돌이 발생하면 충돌이 발생한 오브젝트의
+    // Collider 정보가 넘어온다.
+    // Trigger 를 사용할 때는 Collider 타입으로 매개변수를 받음
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            tileColor2_0_1.material.color = new Color(1f, 1f, 1f, 0.2f);
+        }
+    }
+}
