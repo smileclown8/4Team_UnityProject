0dad00cbb7179b344e4296017cab35ba3df297f5
smileclown8 <77978713+smileclown8@users.noreply.github.com>
2021-02-03 02:02:57 +0900

    2-3 BGM 조정
    2_3 BGM 트리거 위치 수정

---
+++ Bug_hunter/Assets/UI/Bgm_Off.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bgm_Off : MonoBehaviour
+{
+    BGMManager BGM;
+    public int playMusicTrack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM = FindObjectOfType<BGMManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            BGM.Stop();
+            //this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Bug_hunter/Assets/UI/Bgm_On.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bgm_On : MonoBehaviour
+{
+    BGMManager BGM;
+    public int playMusicTrack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM = FindObjectOfType<BGMManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            BGM.Play(playMusicTrack);
+            BGM.SetVolumn(0.2f);
+        }
+    }
+}
