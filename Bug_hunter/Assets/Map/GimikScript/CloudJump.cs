7c6e1efeb5472e241dfa503db1f567eeeb692e01
smileclown8 <77978713+smileclown8@users.noreply.github.com>
2021-02-05 05:37:04 +0900

    2스테이지 전체 레벨링 수정
    완료

---
+++ Bug_hunter/Assets/Map/GimikScript/CloudJump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudJump : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public float JumpForce = 110.0f;
+
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+
+                player.GetComponent<Rigidbody2D>().AddForce(Vector3.up * JumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Bug_hunter/Assets/Map/GimikScript/NoiseGravity.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseGravity : MonoBehaviour
+{
+    public GameObject targetObject;
+    GameObject player;
+    Rigidbody2D rb;
+    public float gravity;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        player = GameObject.Find("player");
+        rb = player.GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        StartCoroutine(Nosing());
+    }
+    IEnumerator Nosing()
+    {
+        while (true)
+        {
+            OnEnableSettingEnvent();
+            yield return new WaitForSeconds(3);//WaitForSeconds객체를 생성해서 반환
+
+
+        }
+    }
+    public void OnEnableSettingEnvent()
+    {
+        if (targetObject.activeInHierarchy)
+        {
+            targetObject.SetActive(false);
+            rb.gravityScale = gravity;
+        }
+        else
+            targetObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            targetObject.SetActive(false);
+            rb.gravityScale = gravity;
+            this.gameObject.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
