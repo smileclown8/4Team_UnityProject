70d75896655c4ddb9db583072b99e620d7c9aef8
hakyung1227 <74641325+hakyung1227@users.noreply.github.com>
2021-02-06 17:46:15 +0900

    메인화면UI작업
    커밋

---
+++ Bug_hunter/Assets/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnClickOnlineButton()
+    {
+        Debug.Log("Click Online");
+    }
+    public void OnClickQuitButton()
+    {
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#else
+            Application.Quit();
+#endif
+    }
+
+}
