9c7b37995901625ed8e3825c524cb0d7a54ae97a
patapon54 <65967006+patapon54@users.noreply.github.com>
2021-02-06 16:23:54 +0900

    각 씬 이동 완료

--- Bug_hunter/Assets/Base/씬 이동/Boss2_GameOver.cs
+++ Bug_hunter/Assets/Base/씬 이동/Boss2_GameOver.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Boss2_GameOver : MonoBehaviour
 {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +22,34 @@ public class Boss2_GameOver : MonoBehaviour
         {
             if (GameObject.Find("PlayerStatusManager").GetComponent<PlayerStatusManager>().player_HP <= 0)
             {
-                Debug.Log("보스방에서 사망");
                 GameOver();
             }
         }
 
 
-        Debug.Log(GameObject.Find("PlayerStatusManager").GetComponent<PlayerStatusManager>().PlayerRespawn_Pos);
+        if (isMoved)
+        {
+            Invoke("MoveToSavePoint", 0.1f);
+            isMoved = false;
+        }
     }
+    private bool isMoved = false;
 
+    void MoveToSavePoint()
+    {
+        if (GameObject.Find("2_5SavePoint_Off") != null)
+        {
+            GameObject.Find("player").transform.position = GameObject.Find("2_5SavePoint_Off").transform.position;
+            Destroy(this.gameObject);
+        }
+    }
 
     void GameOver()
     {
-
+        isMoved = true;
+        GameObject.Find("PlayerStatusManager").GetComponent<PlayerStatusManager>().player_HP
+           = GameObject.Find("PlayerStatusManager").GetComponent<PlayerStatusManager>().player_MaxHP;
         SceneManager.LoadScene("2Stage_Map");
-        GameObject.Find("player").transform.position = GameObject.Find("PlayerStatusManager").GetComponent<PlayerStatusManager>().PlayerRespawn_Pos;
 
     }
 }
